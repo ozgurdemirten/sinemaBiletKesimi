1d538b1f4617a60bec0720c5f22fd77ee100e16c
ozgurdemirten <ozgur1996c@gmail.com>
2023-06-19 02:31:40 +0300

    bittigi kadar

--- javascript_SinemaBileti.js
+++ javascript_SinemaBileti.js
@@ -1,11 +1,11 @@
 const koltuklar = document.querySelector(".koltuk-component");
 let clickKoltuk = document.querySelectorAll(".th-click");
 const uElementiAdet = document.querySelector(".bottom-display-adet");
-const uElementiUcret=document.querySelector(".bottom-display-coin-u")
+const uElementiUcret = document.querySelector(".bottom-display-coin-u");
 let id = 0;
 let idArray = [];
 let idArrayKirmizi = [];
-
+let temp=[];
 class koltukSecimi {
 
     displayKoltuk() {
@@ -53,60 +53,131 @@ class koltukSecimi {
         let degisken = Storage.getKoltuk().length;
         return degisken;
     }
-    keyDown(){
+    keyDown() {
+
         
-        
-        
+       // const degiskenbilal=[15,14,13];
+       
         clickKoltuk = koltuklar.querySelectorAll(".th-click");
         //console.log(idArray);
-        document.addEventListener("keydown", event=> {
-            idArrayKirmizi = Storage.getKoltuk();
-
-            console.log("idarraykirimizi",idArrayKirmizi);
-            if(event.key==="Enter" && idArrayKirmizi.length!=null){
-                console.log(event.key);
+        
+        document.addEventListener("keydown", event => {
+            idArrayKirmizi =Storage.getKoltuk();
+            
+            
+           
+            // console.log("idarray",idArray);
+            // console.log(idArrayKirmizi);
+            
+            // console.log("idarraykirimizi", idArrayKirmizi);
+            if (event.key === "Enter" && idArrayKirmizi.length >0) {
+                //console.log(event.key);
+                
                 //console.log(idArray);
+                
+                
                 idArrayKirmizi.forEach(element => {
-                    //console.log(clickKoltuk[element]);
-                   // console.log(clickKoltuk[element].dataset.id);
+                   // console.log(clickKoltuk[element]);
+                    // console.log(clickKoltuk[element].dataset.id);
+                    
                     idArrayKirmizi.push(clickKoltuk[element].dataset.id);
                     clickKoltuk[element].setAttribute("style", "background-color:red;");
+
                     
-                    this.colorChangeKeyDown(idArray,idArrayKirmizi,clickKoltuk[element],clickKoltuk[element].dataset.id,"red");
+                    
                 });
-               
-                
+                idArrayKirmizi=Array.from(new Set(idArrayKirmizi));
+                for (const iterator of idArray) {
+                    idArrayKirmizi.push(iterator);
+                    
+                }
+                idArray=[];
+                this.colorChangeKeyDown(idArray, idArrayKirmizi);
+                uElementiAdet.innerHTML = 0;
+        uElementiUcret.innerHTML = 0;
             }
            
             
             
-          })
-          
-          
+        })
+        
+
     }
-    colorChangeKeyDown(idArray,idArrayKirmizi,eventTarget,id,renk){
-       {
-            idArrayKirmizi = Storage.getKoltuk();
-           // console.log("array kirmizi",idArrayKirmizi);
-            eventTarget = eventTarget.style.backgroundColor = "red";
-            console.log("id array üst",idArray);
+    siralama(dizi){
+        return dizi.sort(function(a, b) {
+             a - b;
+          });
+    }
+    karsilastirma(a,b){
+        
+       
+           
+         
+            // If length is not equal
+            if (a.length != b.length)
+                return "False";
+            else {
+         
+                // Comparing each element of array
+                for (let i = 0; i < a.length; i++)
+                    if (a[i] != b[i])
+                        return "False";
+                return "True";
             
-            console.log("id array",idArray);
-            this.constLocalStorage(idArrayKirmizi);
+        }
+        
+    }
+    mergeArray(a,b){
+        return a.concat(b);
+    }
+    colorChangeKeyDown(idArray, idArrayKirmizi) {
+        {
+           // console.log("degiskenbilal",degiskenbilal);
+           
+        //     idArray= Storage.getKoltuk();
+        //    //console.log(temp);
+        //    idArray=this.siralama(idArray);
+          
+        //   idArrayKirmizi=this.siralama(idArrayKirmizi);
+        //   console.log(idArray);
+        //   console.log(idArrayKirmizi);
+        //     idArrayKirmizi=this.mergeArray(idArray,idArrayKirmizi);
+          
+          
+           
+               
             
+           
+           
+           
+        //     idArrayKirmizi=Array.from(new Set(idArrayKirmizi));
+
+
+           
+        idArrayKirmizi=Array.from(new Set(idArrayKirmizi));
+           
+            // console.log("array kirmizi",idArrayKirmizi);
+           // eventTarget = eventTarget.style.backgroundColor = "red";
+           // console.log("id array üst", idArray);
+
+           // console.log("id array", idArray);
+           this.constLocalStorage(idArrayKirmizi);
+           
+           
+
+        
             
-            this.colorChangeLocalStorage(idArray);
         }
     }
     displayClick() {
         clickKoltuk = koltuklar.querySelectorAll(".th-click");
-        let renk="lightyellow"
+        let renk = "lightyellow"
         //console.log(clickKoltuk);
         koltuklar.addEventListener("click", event => {
             // console.log(event.target.dataset.id);
             event.target.dataset.id;
-           // console.log(event.target);
-            this.colorChange(event.target, event.target.dataset.id,renk);
+            // console.log(event.target);
+            this.colorChange(event.target, event.target.dataset.id, renk);
             //console.log(event.target.style.backgroundColor);
             this.biletUcret();
 
@@ -115,28 +186,28 @@ class koltukSecimi {
 
     }
 
-    colorChange(eventTarget, id,renk) {
+    colorChange(eventTarget, id, renk) {
         idArray = Storage.getKoltuk();
-       // console.log(eventTarget.style.backgroundColor != renk);
-        
-        if ( eventTarget.dataset.id != null && eventTarget.style.backgroundColor != "lightyellow"&& eventTarget.style.backgroundColor != "red") {
+        // console.log(eventTarget.style.backgroundColor != renk);
+
+        if (eventTarget.dataset.id != null && eventTarget.style.backgroundColor != "lightyellow" && eventTarget.style.backgroundColor != "red") {
             eventTarget = eventTarget.style.backgroundColor = renk;
             idArray.push(id);
             this.colorChangeLocalStorage(idArray);
-            
+
         }
         // else if(eventTarget.dataset.id != null && eventTarget.style.backgroundColor==="lightyellow"){
 
         //     eventTarget = eventTarget.style.backgroundColor = "red";
-            
+
         //     this.colorChangeLocalStorage(idArray);
         // }
-        else if (eventTarget.dataset.id != null && eventTarget.style.backgroundColor != "red" ) {
+        else if (eventTarget.dataset.id != null && eventTarget.style.backgroundColor != "red") {
             eventTarget = eventTarget.style.backgroundColor = "green";
             idArray.pop(id);
             this.colorChangeLocalStorage(idArray);
         }
-        
+
 
 
     }
@@ -146,43 +217,44 @@ class koltukSecimi {
     constLocalStorage(idArray) {
         Storage.saveKoltuklarKalici(idArray);
     }
-    colorStatementGetStorage(idArrayKirmizi) {
+    colorStatementGetStorage() {
         let degisken = Storage.getKoltuk();
-       let  kaliciKoltuk=Storage.getKoltukKalici();
-       //console.log(kaliciKoltuk);
-       // console.log("burasi kirmizi array",idArrayKirmizi);
+        let kaliciKoltuk = Storage.getKoltukKalici();
+        //console.log(kaliciKoltuk);
+        // console.log("burasi kirmizi array",idArrayKirmizi);
         clickKoltuk.forEach(element => {
             //console.log(element);
             let id1 = degisken.find((d) => d === element.dataset.id);
             let id2 = kaliciKoltuk.find((d) => d === element.dataset.id);
-            if(id1 !== element.dataset.id){
+            if (id1 !== element.dataset.id) {
                 element.setAttribute("style", "background-color:green;");
             }
-            
-            if (id1 === element.dataset.id ) {
+
+            if (id1 === element.dataset.id) {
                 element.setAttribute("style", "background-color:LightYellow;");
                 //console.log("buraya girdi yellow");
             }
-            if(id2===element.dataset.id ){
-                
+            if (id2 === element.dataset.id) {
+
                 element.setAttribute("style", "background-color:red;");
             }
-            
+
 
         });
+        
         //console.log(clickKoltuk[11].dataset.id);
 
 
     }
-   
-  biletUcret(){
-        let adet=this.biletSayisi();
-        let sayisi=35*this.biletSayisi();
+
+    biletUcret() {
+        let adet = this.biletSayisi();
+        let sayisi = 35 * this.biletSayisi();
         console.log(sayisi);
-        uElementiAdet.innerHTML=adet;
-        uElementiUcret.innerHTML=sayisi;     
-          
-        }
+        uElementiAdet.innerHTML = adet;
+        uElementiUcret.innerHTML = sayisi;
+
+    }
 
 }
 
@@ -202,10 +274,10 @@ class Storage {
     // static saveCart(cart) {
     //     localStorage.setItem("cart", JSON.stringify(cart));
     // }
-    static getKoltuk() {
+    static  getKoltuk() {
         return localStorage.getItem("koltuklar") ? JSON.parse(localStorage.getItem("koltuklar")) : [];
     }
-    static getKoltukKalici() {
+    static  getKoltukKalici() {
         return localStorage.getItem("koltuklarKalici") ? JSON.parse(localStorage.getItem("koltuklarKalici")) : [];
     }
 }
@@ -213,12 +285,13 @@ class Storage {
 document.addEventListener("DOMContentLoaded", () => {
 
     const koltukSecimi1 = new koltukSecimi();
-    
-    
+
+
     koltukSecimi1.displayKoltuk();
     koltukSecimi1.displayClick();
-     
-    koltukSecimi1.colorStatementGetStorage(idArrayKirmizi);
-       
     koltukSecimi1.keyDown();
+    koltukSecimi1.colorStatementGetStorage();
+
+    
 });
+const koltukSecimi1 = new koltukSecimi();
